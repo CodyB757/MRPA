9ac1ff6aab7808d282b2bc8bfe11f98889177b95
CodyB757 <crb23672@email.vccs.edu>
2026-09-15 13:56:36 -0400

    Add project files.

---
+++ MRPA.cpp
@@ -0,0 +1,21 @@
+// Cody Blake
+// CSC 222
+// Marathon Runners Parallel Arrays Project (MRPA)
+//September 10th, 2026
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+const int NUM_RUNNERS = 5;
+const int NUM_DAYS = 7;
+
+int main()
+{
+    cout << "Marathon Runners Program" << endl;
+
+    return 0;
+}
